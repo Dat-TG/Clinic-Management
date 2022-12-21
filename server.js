26010265792656dc9e163e2e471e3c08ff171dfa
Haihung2101 <hung.91.2016.2017@gmail.com>
2022-12-21 16:54:52 +0700

    Lap phieu kham

--- server.js
+++ server.js
@@ -77,8 +77,6 @@ app.use('/', async(req, res, next) => {
     }
 });
 
-
-
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode | 500;
     res.status(statusCode).send(err.message);
