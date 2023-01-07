e3d00e4dd42eba30dd638fd7dde6b17bda0ee166
20120454 <dat13102k2@gmail.com>
2023-01-07 22:05:05 +0700

    fix bugs

--- model/Database.m.js
+++ model/Database.m.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://NMCNPM:Nhom21@sandbox.2jh56pc.mongodb.net/test";
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri);
 const db = client.db('ClinicManagement');
 db.collection('PatientsInDay').createIndex( { "expireAt": 1 }, { expireAfterSeconds: 0 } );
